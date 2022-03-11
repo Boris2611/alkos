ea7f2873feb734b4794810fdd73e9a87bd2ff477
Boris2611 <84404796+Boris2611@users.noreply.github.com>
2022-03-11 18:15:28 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const check_age = () => {
+    if (document.getElementById('age').checked) {
+        alert("ОК");
+    } else {
+        alert("Нисте потврдили да имате преко 18 година !");
+    }
+};
