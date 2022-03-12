34056079ac4d3af9f11a90d4aded894a8ee7502a
Boris2611 <84404796+Boris2611@users.noreply.github.com>
2022-03-12 14:06:41 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,42 @@ const check_age = () => {
         alert("Нисте потврдили да имате преко 18 година !");
     }
 };
+
+
+
+let brojac1 = 0;
+let brojac2 = 0;
+let brojac3 = 0;
+
+const pokazi_piva = () => {
+    brojac1 += 1;
+
+    if(brojac1 % 2 == 0){
+        document.getElementById("piva").style.display = "none";
+    }
+    else {
+        document.getElementById("piva").style.display = "block";
+    }
+};
+
+const pokazi_rakije = () => {
+    brojac2 += 1;
+
+    if(brojac2 % 2 == 0){
+        document.getElementById("rakije").style.display = "none";
+    }
+    else {
+        document.getElementById("rakije").style.display = "block";
+    }
+};
+
+const pokazi_vina = () => {
+    brojac3 += 1;
+
+    if(brojac3 % 2 == 0){
+        document.getElementById("vina").style.display = "none";
+    }
+    else {
+        document.getElementById("vina").style.display = "block";
+    }
+};
